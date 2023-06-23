faea91c6a81eec673338bb8954529823500dce54
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-06-23 17:49:05 +0300

    playlist.py

--- AlexaMusic/plugins/play/playlist.py
+++ AlexaMusic/plugins/play/playlist.py
@@ -34,7 +34,10 @@ PLAYLIST_COMMAND = get_command("PLAYLIST_COMMAND")
 DELETEPLAYLIST_COMMAND = get_command("DELETEPLAYLIST_COMMAND")
 
 
-@app.on_message(filters.command(PLAYLIST_COMMAND) & ~filters.edited & ~BANNED_USERS)
+@app.on_message(
+ filters.command(PLAYLIST_COMMAND,"")
+    & ~filters.edited
+    & ~BANNED_USERS)
 @language
 async def check_playlist(client, message: Message, _):
     _playlist = await get_playlist_names(message.from_user.id)
@@ -65,7 +68,6 @@ async def check_playlist(client, message: Message, _):
 
 @app.on_message(
     filters.command(DELETEPLAYLIST_COMMAND)
-    & filters.group
     & ~filters.edited
     & ~BANNED_USERS
 )
